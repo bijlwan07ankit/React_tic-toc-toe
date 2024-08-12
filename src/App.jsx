5a4942bf3aed4c45e5e3e239bffe1b2fe7771e7c
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2024-08-12 14:01:42 +0530

    Added X and O  working Button  till ch 82

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import Player from "./Components/Player";
+import {useState} from 'react';
+import GameBoard from "./Components/GameBoard";
+import Log from './Components/Log';
 function App() {
-  
+const [gameTurns,setGameturns]=useState([]);
+  const [activePlayer,setActivePlayer]=useState('X');
 
+  function handleSelectSquare(){
+    setActivePlayer((curActivePlayer)=> curActivePlayer==="X" ? "O" :"X");
+    };
+  
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="player1" symbol="X"/>
-          <Player name="player2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="player1" symbol="X" isActive={activePlayer==='X'}/>
+          <Player initialName="player2" symbol="O" isActive={activePlayer==='O'}/>
           </ol>
-        GAMEBOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
-      LOG 
+      <Log />
     </main>
   )
 }
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import {useState} from 'react'
-function Player({initialName,symbol}) {
+
+function Player({initialName,symbol,isActive}) {
    const [playerName , setPlayerName]= useState(initialName);
-const [isEditting , setIsEditting]=useState(false);
+   const [isEditting , setIsEditting]=useState(false);
+
 function handleEditClick(){ 
     // setIsEditting(!isEditting);  //to toggle value normaly
     // setIsEditting(isEditting ? false : true);  //to toggle value it is too complex 
-    setIsEditting((editing)=>!editing)
-} 
+    setIsEditting((editing)=>!editing);   //best practice when working with element is changing besed on previous value
+}
+
+function handleChange(event){
+  setPlayerName(event.target.value);
+}
+    let editatblePlayerName= (<span className="player-name">{playerName}</span>);
+
+    if(isEditting){
+      editatblePlayerName = (<input type="text" required value={playerName} onChange={handleChange} />)
+    }
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
             <span className="player">
-          {isEditting?(<input type="text" required defaultValue={name} />):(<span className="player-name">{name}</span>)}
+              {editatblePlayerName}
+          {/* {isEditting?(<input type="text" required Value={name} />):(<span className="player-name">{name}</span>)} */}
           <span className="player-symbol">{symbol}</span>
           </span>
           <button onClick={handleEditClick}>{isEditting?("Save"):("Edit")}</button>
