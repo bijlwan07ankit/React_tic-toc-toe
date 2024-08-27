e770cda3386b5dc6b38830c812caa2309518c2ef
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2024-08-27 19:54:48 +0530

    updating stuff

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -7,14 +7,14 @@ const initialGameBoard=[
 export default function GameBoard({onSelectSquare}) {
   //  const [gameBoard, setGameBoard]= useState(initialGameBoard);
 
-  //  function handleSelectSqure(rowIndex, colIndex){
-  //   setGameBoard((prevGameBoard)=>{
-  //       const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
-  //       updatedBoard[rowIndex][colIndex]=activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSquare();
-  //  }
+   function handleSelectSqure(rowIndex, colIndex){
+    setGameBoard((prevGameBoard)=>{
+        const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
+        updatedBoard[rowIndex][colIndex]=activePlayerSymbol;
+      return updatedBoard;
+    });
+    onSelectSquare();
+   }
   return (
     <ol id="game-board">
        {gameBoard.map((row,rowIndex)=>(
